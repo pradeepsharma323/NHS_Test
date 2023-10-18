fdf74b2da1b4946bacd3711670c5cea6d55170c5
pshar456 <pradeep.sharma5@aexp.com>
2023-10-18 09:28:44 +0100

    Initial commit

---
+++ src/main/java/com/nhs/main/Greeter.java
@@ -0,0 +1,25 @@
+package com.nhs.main;
+
+public class Greeter {
+    public static void main(String[] args) {
+
+    }
+
+    public static String greet(String name){
+
+        //2. trim the input
+
+        //3. capitalizeds the first letter of the name
+
+        //4. returns Good Morning <name> when the time is 06:00-12:00
+
+        //5. returns Good evening <name> when the time is 18:00-12:00
+
+        //6. returns Good night <name> when the time is 22:00-06:00
+
+
+        //7. greet logs to console each time ti is called
+
+        return "Hello"+name;
+    }
+}
